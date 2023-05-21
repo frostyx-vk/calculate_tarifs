cd11c7b2f30233c806c88e67398d0982d7d9fbb4
frostyx-vk <vk.xam@yandex.ru>
2023-05-21 20:01:48 +0800

    add discount shift, grade, disability and in month

--- js/calculate_order.js
+++ js/calculate_order.js
@@ -36,60 +36,21 @@ const calcTotalPriceFormset = document.querySelector('.calc-formset__price-cost'
 
 const calculateTotalPriceBlock = document.querySelector('.calc-formset__price-result');
 
-// получение скидки в переменную из JSON формата
-const discountsElement = document.getElementById('prices-discounts');
-const discounts = JSON.parse(discountsElement.getAttribute('data-discounts'));
-
-// скидка за 2 смену
-let twoShiftDiscount;
-for (const discount of discounts) {
-  if (discount.fields.tag === 'second-shift') {
-    twoShiftDiscount = discount.fields.percentage_value ? discount.fields.percentage_value : discount.fields.fixed_value;
-  }
-};
-// скидка от 4 до 6 месяцев
-let fourSixMonthsDiscount;
-for (const discount of discounts) {
-  if (discount.fields.tag === 'four-six-months') {
-    fourSixMonthsDiscount = discount.fields.percentage_value ? discount.fields.percentage_value : discount.fields.fixed_value;
-  }
-};
-
-// скидка от 7 до 9 месяцев
-let sevenNineMonthsDiscount;
-for (const discount of discounts) {
-  if (discount.fields.tag === 'seven-nine-months') {
-    sevenNineMonthsDiscount = discount.fields.percentage_value ? discount.fields.percentage_value : discount.fields.fixed_value;
-  }
-};
-
-// скидка 10% на инвалидность
-let tenDisabilityDiscount;
-for (const discount of discounts) {
-  if (discount.fields.tag === 'disability') {
-    tenDisabilityDiscount = discount.fields.percentage_value ? discount.fields.percentage_value : discount.fields.fixed_value;
-  }
-};
-
-// получение базовой цены данных с бэка
-const basePriceFromBackendData = document.getElementById('prices-discounts');
-const basePriceFromBackend = basePriceFromBackendData.dataset.price;
-
 // базовая цена в зависимости от выбранного калькулятора
 let basePrice;
-if (window.location.pathname.split('/').pop() === 'with-enroll') {
-  basePrice = parseInt(basePriceFromBackend);
+if (window.location.pathname.split('/').pop() === 'calculate_order.html') {
+  basePrice = '8000';
   calcWithEnrollTotalPriceWithDiscount.innerText = formatter.format(Math.round(basePrice)) + ' ₽';
-  calcWithEnrollTotalPriceWithoutDiscount.innerText = formatter.format(Math.round(basePrice)) + ' ₽';
-  calculateTotalPriceBlock.innerText = formatter.format(Math.round(basePrice)) + ' ₽';
-} else if (window.location.pathname.split('/').pop() === 'without-enroll') {
-  basePrice = parseInt(basePriceFromBackend);
+  // calcWithEnrollTotalPriceWithoutDiscount.innerText = formatter.format(Math.round(basePrice)) + ' ₽';
+  // calculateTotalPriceBlock.innerText = formatter.format(Math.round(basePrice)) + ' ₽';
+} else if (window.location.pathname.split('/').pop() === 'without-enroll.html') {
+  basePrice = '7000';
   calcWithEnrollTotalPriceWithDiscount.innerText = formatter.format(Math.round(basePrice)) + ' ₽';
-  calcWithEnrollTotalPriceWithoutDiscount.innerText = formatter.format(Math.round(basePrice)) + ' ₽';
-  calculateTotalPriceBlock.innerText = formatter.format(Math.round(basePrice)) + ' ₽';
+  // calcWithEnrollTotalPriceWithoutDiscount.innerText = formatter.format(Math.round(basePrice)) + ' ₽';
+  // calculateTotalPriceBlock.innerText = formatter.format(Math.round(basePrice)) + ' ₽';
 }
 
-calcTotalPriceFormset.innerText = formatter.format(Math.round(basePrice)) + ' ₽';
+// calcTotalPriceFormset.innerText = formatter.format(Math.round(basePrice)) + ' ₽';
 
 // кнопки смены
 const inputsRadioShift = document.querySelectorAll('input[name="shift"]');
@@ -102,12 +63,9 @@ inputRadioShiftTwo.dataset.value = 0.9;
 // кнопки класса
 const inputsRadioGrade = document.querySelectorAll('input[name="grade"]');
 const inputRadioGradeOne = document.querySelector('#id_grade_0');
+const inputRadioGradeTwo = document.querySelector('#id_grade_1');
 inputRadioGradeOne.dataset.value = 1;
-
-// получение цены за 5-11 класс и разницы между ними с бэка
-const basePriceFromBackendData511 = document.getElementById('prices-discounts');
-const basePriceFromBackend511 = basePriceFromBackendData511.dataset.price511;
-const markupForGrade = parseInt(basePriceFromBackend511) - parseInt(basePriceFromBackend);
+inputRadioGradeTwo.dataset.value = 1.1;
 
 // кнопка нивалидности
 const inputCheckboxDisability = document.querySelector('#id_disability');
@@ -119,59 +77,50 @@ function calculateWithEnrollWithDiscount() {
   let totalPriceWithDiscount = basePrice * parseInt(inputRangeWithEnroll.value);
 
   if (inputRangeWithEnroll.value > 3) {
-    totalPriceWithDiscount = basePrice * parseInt(inputRangeWithEnroll.value) * (1 - fourSixMonthsDiscount / 100);
+    totalPriceWithDiscount = basePrice * parseInt(inputRangeWithEnroll.value) * 0.95;
   };
   if (inputRangeWithEnroll.value > 6) {
-    totalPriceWithDiscount = basePrice * parseInt(inputRangeWithEnroll.value) * (1 - sevenNineMonthsDiscount / 100);
+    totalPriceWithDiscount = basePrice * parseInt(inputRangeWithEnroll.value) * 0.93;
   };
 
-  for (let i = 1; i < inputsRadioShift.length; i++) {
-    if ((inputsRadioShift[1].checked) && (inputRangeWithEnroll.value > 6)) {
-      totalPriceWithDiscount = (totalPriceWithDiscount - (parseInt(twoShiftDiscount)) * (1 - sevenNineMonthsDiscount / 100) * parseInt(inputRangeWithEnroll.value));
-    } else if ((inputsRadioShift[1].checked) && (inputRangeWithEnroll.value > 3)) {
-      totalPriceWithDiscount = (totalPriceWithDiscount - (parseInt(twoShiftDiscount)) * (1 - fourSixMonthsDiscount / 100) * parseInt(inputRangeWithEnroll.value));
-    } else if (inputsRadioShift[1].checked) {
-      totalPriceWithDiscount = totalPriceWithDiscount - (parseInt(twoShiftDiscount) * parseInt(inputRangeWithEnroll.value));
-    };
+  for (const inputRadioShift of inputsRadioShift) {
+    if (inputRadioShift.checked) {
+      totalPriceWithDiscount = totalPriceWithDiscount * inputRadioShift.dataset.value;
+ };
   };
 
-  for (let i = 1; i < inputsRadioGrade.length; i++) {
-    if ((inputsRadioGrade[1].checked) && (inputRangeWithEnroll.value > 6)) {
-      totalPriceWithDiscount = totalPriceWithDiscount + markupForGrade * (1 - sevenNineMonthsDiscount / 100) * parseInt(inputRangeWithEnroll.value);
-    } else if ((inputsRadioGrade[1].checked) && (inputRangeWithEnroll.value > 3)) {
-      console.log(markupForGrade);
-      totalPriceWithDiscount = totalPriceWithDiscount + markupForGrade * (1 - fourSixMonthsDiscount / 100) * parseInt(inputRangeWithEnroll.value);
-    } else if (inputsRadioGrade[1].checked) {
-      totalPriceWithDiscount = totalPriceWithDiscount + (markupForGrade * parseInt(inputRangeWithEnroll.value));
+  for (const inputRadioGrade of inputsRadioGrade) {
+    if (inputRadioGrade.checked) {
+      totalPriceWithDiscount = totalPriceWithDiscount * inputRadioGrade.dataset.value;
     };
   };
 
   if (inputCheckboxDisability.checked) {
-    totalPriceWithDiscount = totalPriceWithDiscount * (1 - tenDisabilityDiscount / 100);
+    totalPriceWithDiscount = totalPriceWithDiscount * inputCheckboxDisability.dataset.value;
   }
 
   totalPriceTarif = totalPriceWithDiscount;
   calcWithEnrollTotalPriceWithDiscount.innerText = formatter.format(Math.round(totalPriceWithDiscount)) + ' ₽';
-  calcTotalPriceFormset.innerText = calcWithEnrollTotalPriceWithDiscount.innerText;
+  // calcTotalPriceFormset.innerText = calcWithEnrollTotalPriceWithDiscount.innerText;
 };
 
-function calculateWithEnrollWithoutDiscount() {
-  let totalPriceWithoutDiscount = basePrice * parseInt(inputRangeWithEnroll.value);
+// function calculateWithEnrollWithoutDiscount() {
+//   let totalPriceWithoutDiscount = basePrice * parseInt(inputRangeWithEnroll.value);
 
-  for (let i = 1; i < inputsRadioGrade.length; i++) {
-    if (inputsRadioGrade[1].checked) {
-      totalPriceWithoutDiscount = totalPriceWithoutDiscount + (markupForGrade * parseInt(inputRangeWithEnroll.value));
-    };
-  };
+//   for (let i = 1; i < inputsRadioGrade.length; i++) {
+//     if (inputsRadioGrade[1].checked) {
+//       totalPriceWithoutDiscount = totalPriceWithoutDiscount + (markupForGrade * parseInt(inputRangeWithEnroll.value));
+//     };
+//   };
 
-  calcWithEnrollTotalPriceWithoutDiscount.innerText = formatter.format(totalPriceWithoutDiscount) + ' ₽';
-};
+//   calcWithEnrollTotalPriceWithoutDiscount.innerText = formatter.format(totalPriceWithoutDiscount) + ' ₽';
+// };
 
-function calculateWithEnrollDiscount() {
-  const totalPriceDiscount = calcWithEnrollTotalPriceWithoutDiscount.innerText.replace(/\D/g, '') - calcWithEnrollTotalPriceWithDiscount.innerText.replace(/\D/g, '');
+// function calculateWithEnrollDiscount() {
+//   const totalPriceDiscount = calcWithEnrollTotalPriceWithoutDiscount.innerText.replace(/\D/g, '') - calcWithEnrollTotalPriceWithDiscount.innerText.replace(/\D/g, '');
 
-  calcWithEnrollTotalPriceDiscount.innerText = 'Скидка: ' + formatter.format(totalPriceDiscount) + ' ₽';
-};
+//   calcWithEnrollTotalPriceDiscount.innerText = 'Скидка: ' + formatter.format(totalPriceDiscount) + ' ₽';
+// };
 
 // расчет услуг
 const formsetOptionsContainers = document.querySelectorAll('.payment_months-wrapper input');
@@ -220,20 +169,20 @@ function calculateWithEnrollOptions() {
   totalResultOptionsPrice = allCalcOptionsTotalPrice.reduce((partialSum, a) => partialSum + parseInt(a.innerText.replace(/\s+/g, '')), 0);
 
   const calcTotalPriceOptioons = document.querySelector('.calc-formset__price-options');
-  calcTotalPriceOptioons.innerText = formatter.format(totalResultOptionsPrice) + ' ₽';
+  // calcTotalPriceOptioons.innerText = formatter.format(totalResultOptionsPrice) + ' ₽';
 };
 
 function calculateTotalPrice() {
   const calculateTotalPrice = totalPriceTarif + totalResultOptionsPrice;
 
-  calculateTotalPriceBlock.innerText = formatter.format(calculateTotalPrice) + ' ₽';
+  // calculateTotalPriceBlock.innerText = formatter.format(calculateTotalPrice) + ' ₽';
 };
 
 for (const input of inputsWithEnroll) {
   input.addEventListener('input', function() {
     calculateWithEnrollWithDiscount();
-    calculateWithEnrollWithoutDiscount();
-    calculateWithEnrollDiscount();
+    // calculateWithEnrollWithoutDiscount();
+    // calculateWithEnrollDiscount();
     calculateWithEnrollOptions();
     calculateTotalPrice();
   });
